f2a0fb2e4f4e5d0d86706d728da4022e42967d6c
Wang Pengyu <wpy031025@163.com>
2023-12-16 11:04:50 +0800

    1

--- javascript/Ajax预习资料/dy04/03-code/13.案例-学习反馈/js/index.js
+++ javascript/Ajax预习资料/dy04/03-code/13.案例-学习反馈/js/index.js
@@ -3,9 +3,16 @@
  *  1. 获取数据
  *  2. 渲染数据
  * */
+async function province() {
+  const res = await axios({
+    url: "https://hmajax.itheima.net/api/province",
+  })
+  const list = res.data.list
+  document.querySelector(".province").innerHTML += list.map((ele) => `<option value="${ele}">${ele}</option>`).join("")
 
-
-
+  //   document.querySelector(".area").innerHTML = `<option value="">地区</option>`
+}
+province()
 /**
  * 需求2: 城市列表
  *  2.1 注册事件
@@ -13,9 +20,20 @@
  *  2.3 渲染数据
  *  2.4 清空地区列表
  * */
-
-
-
+document.querySelector(".province").addEventListener("change", async function () {
+  const pname = this.value
+  console.log(this.value)
+  const res = await axios({
+    url: "https://hmajax.itheima.net/api/city",
+    params: {
+      pname,
+    },
+  })
+  const list = res.data.list
+  document.querySelector(".city").innerHTML = `<option value="">城市</option>`
+  document.querySelector(".city").innerHTML += list.map((ele) => `<option value="${ele}">${ele}</option>`).join("")
+  document.querySelector(".area").innerHTML = `<option value="">地区</option>`
+})
 
 /**
  * 需求3: 地区列表
@@ -23,7 +41,20 @@
  *  3.2 获取地区数据
  *  3.3 渲染数据
  * */
-
+document.querySelector(".city").addEventListener("change", async function () {
+  const pname = document.querySelector(".province").value
+  const cname = this.value
+  console.log(this.value)
+  const res = await axios({
+    url: "https://hmajax.itheima.net/api/area",
+    params: {
+      pname,
+      cname,
+    },
+  })
+  const list = res.data.list
+  document.querySelector(".area").innerHTML += list.map((ele) => `<option value="${ele}">${ele}</option>`).join("")
+})
 
 /**
  * 需求4: 反馈提交
@@ -31,3 +62,19 @@
  *  4.2 收集表单数据并提交
  *  4.3 提示用户
  * */
+document.querySelector(".btn-secondary").addEventListener("click", async function (e) {
+  const form = document.querySelector("form")
+  const data = serialize(form, { hash: true, empty: true })
+  try {
+    const res = await axios({
+      url: "https://hmajax.itheima.net/api/feedback",
+      method: "post",
+      data,
+    })
+    alert(res.data.message)
+    form.reset()
+  } catch (error) {
+    alert(error.response.data.message)
+    form.reset()
+  }
+})
