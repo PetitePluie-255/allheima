e455146729e60e4fcbbc34c63fc7ebc4607a2d2a
Wang Pengyu <wpy031025@163.com>
2024-01-08 22:09:25 +0800

    2024/1/8

---
+++ vue/dy06/code/vue-router/src/views/NotFind.vue
@@ -0,0 +1,17 @@
+<template>
+  <div><h1>Not Find 404</h1></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h1{
+    width: 400px;
+    text-align: center;
+    margin: 0 auto;
+}
+</style>
